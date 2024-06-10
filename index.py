24bfdcc88ef98ec35be8ab142c4292289cd35a7c
yarden <yarden.tulchinsky@gmail.com>
2024-06-10 13:19:20 +0300

    adding a comments for what to do

---
+++ index.py
@@ -0,0 +1,13 @@
+#create dictionary for poeple and import
+
+#get a random person for comparison A and get a random person for comparison B
+
+#initialise a score
+
+#looping trough game when the guess is correct
+
+#check choice of player if the choice he chose has a higher follower count then the other choice
+
+#if the choice is right up the score by one and get a new random preson for comparison B and put the right choice in comparison A
+
+#if not right end the game
